181237ac3f6db1a88b42dba079894c3da8a9798c
acovachod <166393957+acovachod@users.noreply.github.com>
2025-09-29 12:40:52 +0200

    Avoiding clousures for vars...

--- files/nomad-extip-manager.py
+++ files/nomad-extip-manager.py
@@ -36,7 +36,7 @@ if ! iptables -t nat -S "POSTROUTING" 2>/dev/null | grep -m1 '^-A[[:space:]]\+'P
 	rule_ts="$(date +%s)"
 	iptables -t nat -I POSTROUTING 1 -m comment --comment "NEIM:$rule_ts" -j EXTERNAL_IP
 	# Erasing legacy nomad-extip-manager and docker-extip-manager rules...
-	for neim_rule in $(iptables -L POSTROUTING -n -v -t nat | grep -v "${rule_ts}" | grep -o 'NEIM:[0-9]\+')
+	for neim_rule in $(iptables -L POSTROUTING -n -v -t nat | grep -v "$rule_ts" | grep -o 'NEIM:[0-9]\+')
 	do
 		iptables -t nat -D POSTROUTING -j EXTERNAL_IP -m comment --comment "$neim_rule"
 	done
