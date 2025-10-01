7204cd7f7643fc3464cbf3167f3754ace2d0fe25
acovachod <166393957+acovachod@users.noreply.github.com>
2025-10-01 18:35:00 +0200

    Erasing unnecessary check that leads to a race-condition issue

--- files/nomad-extip-manager.py
+++ files/nomad-extip-manager.py
@@ -33,7 +33,6 @@ done
 
 ts=`date +%s`
 
-iptables -w -t nat -S POSTROUTING | head -n 2 | grep -F -- '-j EXTERNAL_IP' && exit 0 		#< If jump rule exists (at 1st position), we are done
 iptables -w -t nat -I POSTROUTING 1 -m comment --comment "NEIM:$ts" -j EXTERNAL_IP 			#< Insert jump to our target (at 1st position)
 while iptables -t nat -D POSTROUTING -j EXTERNAL_IP >/dev/null 2>&1; do :; done				#< Delete (potential) existing jump rule w/o signature
 
